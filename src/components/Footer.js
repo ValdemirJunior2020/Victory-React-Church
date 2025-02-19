8d745d222da109d62d71c9cb9cccee51a58264e1
ValdemirJunior2020 <your-infojr.83@gmail.com>
2025-02-19 16:02:15 -0500

    new-sherry-video

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,22 +8,22 @@ function Footer() {
   return (
     <div className='footer-container'>
       <section className='footer-subscription'>
-        <p className='footer-subscription-heading'>
-          Join the Adventure newsletter to receive our best vacation deals
-        </p>
+        {/* <p className='footer-subscription-heading'>
+           */}
+        
         <p className='footer-subscription-text'>
-          You can unsubscribe at any time.
+          
         </p>
         <div className='input-areas'>
-          <form>
+          * <form>
             <input
               className='footer-input'
               name='email'
               type='email'
               placeholder='Your Email'
             />
-            <Button buttonStyle='btn--outline'>Subscribe</Button>
-          </form>
+            {/* <Button buttonStyle='btn--outline'>Subscribe</Button> */}
+          </form> 
         </div>
       </section>
       <div className='footer-links'>
