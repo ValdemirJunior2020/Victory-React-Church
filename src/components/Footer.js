44ddeec505793a590b0564f0544c4ff1fb74946c
ValdemirJunior2020 <your-infojr.83@gmail.com>
2025-02-20 15:15:05 -0500

    Footer-Fixed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,115 +1,119 @@
 import React from 'react';
 import './Footer.css';
-import { Button } from './Button';
 import { Link } from 'react-router-dom';
-import { FaFacebookF, FaInstagram, FaYoutube, FaTwitter, FaLinkedin } from 'react-icons/fa'; // ✅ Import React Icons
+import { FaFacebookF, FaInstagram, FaYoutube, FaTwitter, FaLinkedin } from 'react-icons/fa';
 
 function Footer() {
   return (
-    <div className='footer-container'>
-      <section className='footer-subscription'>
-        {/* <p className='footer-subscription-heading'>
-           */}
-        
-        <p className='footer-subscription-text'>
-          
+    <div className="footer-container">
+      <section className="footer-subscription">
+        <p className="footer-subscription-text">
+          {/* Add text here if needed, e.g., "Subscribe to our newsletter" */}
         </p>
-        <div className='input-areas'>
-          * <form>
+        <div className="input-areas">
+          <form>
             <input
-              className='footer-input'
-              name='email'
-              type='email'
-              placeholder='Your Email'
+              className="footer-input"
+              name="email"
+              type="email"
+              placeholder="Your Email"
             />
-            {/* <Button buttonStyle='btn--outline'>Subscribe</Button> */}
-          </form> 
+            {/* Button removed since it’s commented out and unused */}
+          </form>
         </div>
       </section>
-      <div className='footer-links'>
-        <div className='footer-link-wrapper'>
-          <div className='footer-link-items'>
+      <div className="footer-links">
+        <div className="footer-link-wrapper">
+          <div className="footer-link-items">
             <h2>About Us</h2>
-            <Link to='/sign-up'>How it works</Link>
-            <Link to='/'>Testimonials</Link>
-            <Link to='/'>Careers</Link>
-            <Link to='/'>Investors</Link>
-            <Link to='/'>Terms of Service</Link>
+            <Link to="/sign-up">How it works</Link>
+            <Link to="/">Testimonials</Link>
+            <Link to="/">Careers</Link>
+            <Link to="/">Investors</Link>
+            <Link to="/">Terms of Service</Link>
           </div>
-          <div className='footer-link-items'>
+          <div className="footer-link-items">
             <h2>Contact Us</h2>
-            <Link to='/'>Contact</Link>
-            <Link to='/'>Support</Link>
-            <Link to='/'>Destinations</Link>
-            <Link to='/'>Sponsorships</Link>
+            <Link to="/">Contact</Link>
+            <Link to="/">Support</Link>
+            <Link to="/">Destinations</Link>
+            <Link to="/">Sponsorships</Link>
           </div>
         </div>
-        <div className='footer-link-wrapper'>
-          <div className='footer-link-items'>
+        <div className="footer-link-wrapper">
+          <div className="footer-link-items">
             <h2>Videos</h2>
-            <Link to='/'>Submit Video</Link>
-            <Link to='/'>Ambassadors</Link>
-            <Link to='/'>Agency</Link>
-            <Link to='/'>Influencer</Link>
+            <Link to="/">Submit Video</Link>
+            <Link to="/">Ambassadors</Link>
+            <Link to="/">Agency</Link>
+            <Link to="/">Influencer</Link>
           </div>
-          <div className='footer-link-items'>
+          <div className="footer-link-items">
             <h2>Social Media</h2>
-            <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">Instagram</a>
-            <a href="https://www.facebook.com/groups/56003018318" target="_blank" rel="noopener noreferrer">Facebook</a>
-            <a href="https://www.youtube.com/@VictoryWorshipCenter" target="_blank" rel="noopener noreferrer">YouTube</a>
-            <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">Twitter</a>
+            <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+              Instagram
+            </a>
+            <a href="https://www.facebook.com/groups/56003018318" target="_blank" rel="noopener noreferrer">
+              Facebook
+            </a>
+            <a href="https://www.youtube.com/@VictoryWorshipCenter" target="_blank" rel="noopener noreferrer">
+              YouTube
+            </a>
+            <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+              Twitter
+            </a>
           </div>
         </div>
       </div>
-      <section className='social-media'>
-        <div className='social-media-wrap'>
-          <div className='footer-logo'>
-            
+      <section className="social-media">
+        <div className="social-media-wrap">
+          <div className="footer-logo">
+            {/* Add logo or text here if desired */}
           </div>
-          <small className='website-rights'>Victory Worship Center © 2025</small>
-          <div className='social-icons'>
+          <small className="website-rights">Victory Worship Center © 2025</small>
+          <div className="social-icons">
             <a
-              className='social-icon-link facebook'
+              className="social-icon-link facebook"
               href="https://www.facebook.com/groups/56003018318"
-              target='_blank'
+              target="_blank"
               rel="noopener noreferrer"
-              aria-label='Facebook'
+              aria-label="Facebook"
             >
               <FaFacebookF />
             </a>
             <a
-              className='social-icon-link instagram'
+              className="social-icon-link instagram"
               href="https://www.instagram.com"
-              target='_blank'
+              target="_blank"
               rel="noopener noreferrer"
-              aria-label='Instagram'
+              aria-label="Instagram"
             >
               <FaInstagram />
             </a>
             <a
-              className='social-icon-link youtube'
+              className="social-icon-link youtube"
               href="https://www.youtube.com/@VictoryWorshipCenter"
-              target='_blank'
+              target="_blank"
               rel="noopener noreferrer"
-              aria-label='YouTube'
+              aria-label="YouTube"
             >
               <FaYoutube />
             </a>
             <a
-              className='social-icon-link twitter'
+              className="social-icon-link twitter"
               href="https://twitter.com"
-              target='_blank'
+              target="_blank"
               rel="noopener noreferrer"
-              aria-label='Twitter'
+              aria-label="Twitter"
             >
               <FaTwitter />
             </a>
             <a
-              className='social-icon-link linkedin'
+              className="social-icon-link linkedin"
               href="https://www.linkedin.com"
-              target='_blank'
+              target="_blank"
               rel="noopener noreferrer"
-              aria-label='LinkedIn'
+              aria-label="LinkedIn"
             >
               <FaLinkedin />
             </a>
