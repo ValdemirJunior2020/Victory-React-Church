838a1a25931a8d1f54143016f24ba433492f16e8
ValdemirJunior2020 <your-infojr.83@gmail.com>
2025-02-24 14:44:25 -0500

    Enlarger-image

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -3,17 +3,16 @@ import './Cards.css';
 
 function Cards() {
   const videoRef = useRef(null);
-  const [isPlaying, setIsPlaying] = useState(false);
+  const [enlargedMedia, setEnlargedMedia] = useState(null); // State for enlarged media
 
-  const handleVideoClick = () => {
-    if (videoRef.current) {
-      if (isPlaying) {
-        videoRef.current.pause();
-      } else {
-        videoRef.current.play();
-      }
-      setIsPlaying(!isPlaying);
-    }
+  // Function to handle image or video click for enlargement
+  const handleEnlarge = (mediaSrc, type) => {
+    setEnlargedMedia({ src: mediaSrc, type });
+  };
+
+  // Function to close the enlarged media
+  const closeEnlargedMedia = () => {
+    setEnlargedMedia(null);
   };
 
   return (
@@ -34,19 +33,16 @@ function Cards() {
             <li className="cards__item sherry-card">
               <div className="cards__item__link">
                 <figure className="cards__item__pic-wrap" data-category="Experience">
-                <video
-  ref={videoRef}
-  className="cards__item__img sherry-video"
-  onClick={handleVideoClick}  // Make sure it's actually used here
-  controls
-  poster="/images/thumbnail.jpg"
->
-  <source src="/videos/sherry.mp4" type="video/mp4" />
-  Your browser does not support the video tag.
-</video>
-
-
-
+                  <video
+                    ref={videoRef}
+                    className="cards__item__img sherry-video"
+                    onClick={() => handleEnlarge("/videos/sherry.mp4", "video")} // Click to enlarge
+                    controls
+                    poster="/images/thumbnail.jpg"
+                  >
+                    <source src="/videos/sherry.mp4" type="video/mp4" />
+                    Your browser does not support the video tag.
+                  </video>
                 </figure>
                 <div className="cards__item__info">
                   <h5 className="cards__item__text">Our Pastor</h5>
@@ -60,7 +56,12 @@ function Cards() {
             <li className="cards__item">
               <div className="cards__item__link">
                 <figure className="cards__item__pic-wrap" data-category="Adventure">
-                  <img className="cards__item__img" src="/images/img-9.jpg" alt="Adventure" />
+                  <img
+                    className="cards__item__img"
+                    src="/images/img-9.jpg"
+                    alt="Adventure"
+                    onClick={() => handleEnlarge("/images/img-9.jpg", "image")} // Click to enlarge
+                  />
                 </figure>
                 <div className="cards__item__info">
                   <h5 className="cards__item__text">Our Church</h5>
@@ -70,7 +71,12 @@ function Cards() {
             <li className="cards__item">
               <div className="cards__item__link">
                 <figure className="cards__item__pic-wrap" data-category="Mystery">
-                  <img className="cards__item__img" src="/images/img-3.jpg" alt="Mystery" />
+                  <img
+                    className="cards__item__img"
+                    src="/images/img-3.jpg"
+                    alt="Mystery"
+                    onClick={() => handleEnlarge("/images/img-3.jpg", "image")} // Click to enlarge
+                  />
                 </figure>
                 <div className="cards__item__info">
                   <h5 className="cards__item__text">
@@ -84,7 +90,12 @@ function Cards() {
             <li className="cards__item">
               <div className="cards__item__link">
                 <figure className="cards__item__pic-wrap" data-category="Adventure">
-                  <img className="cards__item__img" src="/images/img-4.jpg" alt="Himalayan Adventure" />
+                  <img
+                    className="cards__item__img"
+                    src="/images/img-4.jpg"
+                    alt="Himalayan Adventure"
+                    onClick={() => handleEnlarge("/images/img-4.jpg", "image")} // Click to enlarge
+                  />
                 </figure>
                 <div className="cards__item__info">
                   <h5 className="cards__item__text">
@@ -98,6 +109,22 @@ function Cards() {
           </ul>
         </div>
       </div>
+
+      {/* Lightbox Modal for Enlarged Media */}
+      {enlargedMedia && (
+        <div className="lightbox" onClick={closeEnlargedMedia}>
+          <div className="lightbox-content">
+            {enlargedMedia.type === "image" ? (
+              <img src={enlargedMedia.src} alt="Enlarged" />
+            ) : (
+              <video controls>
+                <source src={enlargedMedia.src} type="video/mp4" />
+                Your browser does not support the video tag.
+              </video>
+            )}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
